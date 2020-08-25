15ee6b982eaef2546e177f2bbfd0d3ce782e5bf2
Algernon-114 <Algernon-114@users.noreply.github.com>
2020-08-25 22:20:14 +0800

    12

--- spot.js
+++ spot.js
@@ -10,15 +10,27 @@
     
     function start() {
         document.getElementById('go').removeEventListener('click', start);
-        promise.then(() => engine.toText('七夕'))
+        promise.then(() => engine.toText('没有准备礼物'))
         .then(() => engine.shake())
-        .then(() => engine.toText('O'))
+        .then(() => engine.toText('就折腾了下这个'))
         .then(() => engine.shake())
-        .then(() => engine.toText('V'))
+        .then(() => engine.toText('折腾了一晚上'))
         .then(() => engine.shake())
-        .then(() => engine.toText('E'))
+        .then(() => engine.toText('还没弄好'))
         .then(() => engine.shake())
-        .then(() => engine.clear())
+        .then(() => engine.toText('我太菜了'))
+        .then(() => engine.shake())
+        .then(() => engine.toText('anyway'))
+        .then(() => engine.shake())
+        .then(() => engine.toText('希望能让你'))
+        .then(() => engine.shake())
+        .then(() => engine.toText('开心一点吧'))
+        .then(() => engine.shake())
+        .then(() => engine.toText('七夕快乐❤'))
+        // .then(() => engine.shake())
+        // .then(() => engine.toText('❤❤❤'))
+        // .then(() => engine.shake())
+        // .then(() => engine.clear())
         .then(() => document.getElementById('go').addEventListener('click', start));
     }
     start()
